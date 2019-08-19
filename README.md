02696fd5fd6edc4db041233c4ad9787290af7912
Billy Ashmall <billy.ashmall@nasa.gov>
2019-08-19 12:24:16 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# Firebase-table-UI-with-CRUD
+# Firebase table UI with CRUD
+
+This application is created to show how to create an editable table display of data being stored in the firebase Cloud Firestore database.
+
+Because we wanted this application to be server agnostic we decided to use firebase to store our data. This eliminates the need to have different setup requirements for different operating systems and also eliminates the need to setup a database. You will have to create a firebase project and then enter the details in the application, but that is all that is needed.
+
+## The setup steps
+
+- Setting up firebase
+- Deploying to your server
+- Adding layers
+
+## Setting up firebase
+
+Start by navigating to https://console.firebase.google.com. If you are not signed in to your google account it will ask you to do so before it takes you to the console.
+
+Once at the console you will: 
+
+- Click the Add project square.
+- Enter mapviewer in the project name box
+- Check both of the checkboxes to accept the terms.
+- Click Create project
+- Click Continue
+- Click the button that looks like </> Add Firebase to your web app
+- Copy the code snippet that is provided, set aside in a text editor and close the dialog box
+- Expand Develop in the left menu panel
+- Click Database
+- Find the Cloud Firestore section (they move it frequently, currently it is at the top)
+- Click Create database
+- Click Enable (leave the default ‘Start in locked mode’)
+- Click rules and remove the code that is there, replace it with the following:
+
+```
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+   function getRole(role) {
+    return get(/databases/$(database)/documents/users/$(request.auth.uid)).data.roles.hasAny([role]);
+  }
+    match /users/{userId} {
+      allow read: if request.auth.uid != null;
+      allow create: if request.auth.uid == userId && request.resource.data.roles.hasAny(["admin"]) == false;
+    }
+    match /book/{bookId} {
+      allow read: if request.auth.uid != null;
+      allow write: if request.auth.uid != null && getRole('admin') == true;
+    }
+  }
+}
+```
+
+## Deploying to your server
+
+On any web server copy the files from https://github.com/billyz313/Firebase-table-UI-with-CRUD into the folder you would like to publish. Rename js/firebase.js.example to js/firebase.js then go to the code snippet that we set aside earlier. Copy everything inside the { } and replace the same in firebase.js (see example below.) Save the file.
+```
+apiKey: "5wUlAI22222Li5gMNesIk9jsnEdTGwfKExymsDQ",
+authDomain: "firebasetableui.firebaseapp.com",
+databaseURL: "https://firebasetableui.firebaseio.com",
+projectId: "firebasetableui",
+storageBucket: "firebasetableui.appspot.com",
+messagingSenderId: "149999999352",
+appId: "1:149999999352:web:12345f11febadf09"
+```
+
+Setup the routing to the application as you would with any application on your server.  (This step is specific to whatever flavor of server you choose.)
+
+## Adding data
+
+Your first step is to run your app and login with your google account.  The login link is at the top right, if it says "Logout" then you have been automatically logged in with your active google account.
+Once you are logged in you will need to visit the firebase console one final time to grant yourself admin rights.  
+
+In the firebase console:
+- Click Data (if you were already there you may have to click Rules, then back to Data)
+- Select users in the collections, there will be one document which was created when you logged in, select that
+- select roles and hover to the right where it shows "Array +" 
+- Click the "+"
+- Type admin in the value box and hit Add
+
+You are now an admin in the app and can add, edit, or remove books.  You will have to logout of the application and log back in to refresh the roles.  
+Once you have logged back in you will notice the Add button which you can click to start adding.  To edit or remove an entry that has been added you simply click the entry in the table and edit or delete it in the dialog box.
+
+All logged in users will be able to view the books, however only users with the admin role will be able to add, edit, or delete.  This application does not go into user management at this point so if you want to add an admin you will have to manually add the admin field to their user roles array.  
+
+You can add the user management to your applications by adding the following security rule to the users path in the firebase security rules 
+```
+allow edit: if request.auth.uid != null && getRole('admin') == true;
+```
+Of course you will have to create a UI to manage them but you could follow this table example to do so!
+Happy coding!
---
+++ js/table-ui.js
@@ -0,0 +1,264 @@
+﻿// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let googleUser;
+const db = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+provider.addScope("https://www.googleapis.com/auth/userinfo.email");
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        $("#btnGoogleLogin").text("Logout");
+        googleUser = user;
+        checkUser();
+        checkwrite();
+
+    } else {
+        $("#btnGoogleLogin").text("Login with Google");
+    }
+});
+
+startLogin = () => {
+    firebase.auth().signInWithPopup(provider).then(result => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const token = result.credential.accessToken;
+        // The signed-in user info.
+        googleUser = result.user;
+        //check if user is in db or add if not
+        checkUser();
+    }).catch(error => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user"s account used.
+        const email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        const credential = error.credential;
+    });
+};
+
+checkUser = () => {
+    const docRef = db.collection("users/").doc(googleUser.uid);
+    docRef.get().then(querySnapshot => {
+        if (querySnapshot.exists) {
+            console.log("I exist");
+        } else {
+            var user = {
+                name: googleUser.displayName,
+                email: googleUser.email,
+                roles: ["user"]
+            };
+            db.collection("users").doc(googleUser.uid).set(user).then(() => {
+                console.log("I now exist");
+            });
+        }
+    }).catch(err => {
+        console.log("login needed");
+    });
+};
+
+logout = () => {
+    firebase.auth().signOut().then(() => {
+        $("#adminForm").hide();
+        $("#userForm").show();
+    }, error => {
+        // An error happened.
+        console.log("Logout error.");
+    });
+};
+
+toggleAuthentication = () => {
+    if (firebase.auth().currentUser !== null) {
+        logout();
+    } else {
+        startLogin();
+    }
+};
+
+checkwrite = () => {
+    try {
+        const docRef = db.collection("users/").doc(googleUser.uid);
+        docRef.get().then(querySnapshot => {
+            if (querySnapshot.data().roles.includes('admin')) {
+                setDtClick();
+                $("#btnAdd").show();
+            } else {
+                removeAdminFunctions();
+            }
+            $("#adminForm").show();
+            $("#userForm").hide();
+            getData();
+        }).catch(err => {
+            console.log("login needed");
+        });
+    }
+    catch (e) {
+        console.log(e);
+        console.log(e.message);
+        console.log("no permissions");
+    }
+};
+
+removeAdminFunctions = () => {
+    dtTable.off('click');
+    $("#btnAdd").hide();
+};
+
+setDtClick = () => {
+    dtTable.on("click", "tr", function () {
+        if ($(this).hasClass("selected")) {
+            $(this).removeClass("selected");
+        } else {
+            dtTable.$("tr.selected").removeClass("selected");
+            $(this).addClass("selected");
+        }
+        if (dtTable.row(this).data()) {
+            const selectedData = dtTable.row(this).data();
+            clearInput();
+            $("#hd-id").val(selectedData.id);
+            $("#booktitle").val(selectedData.title);
+            $("#author").val(selectedData.author);
+            $("#category").val(selectedData.category);
+            $("#isbn").val(selectedData.isbn);
+            $("#price").val(selectedData.price);
+            $("#condition").val(selectedData.condition);
+            $("#myModal").modal("show");
+            $("#btnDelete").show();
+        }
+    });
+};
+
+$(() => {
+    dtTable = $("#dataLayersTable").DataTable({
+        columns: [
+            { data: "title" },
+            { data: "author" },
+            { data: "category" },
+            { data: "isbn" },
+            { data: "price" },
+            { data: "condition" }
+        ]
+    });
+
+
+
+    $("#btnAdd").on("click", () => {
+        clearInput();
+        $("#myModal").modal("show");
+        $("#btnDelete").hide();
+    });
+
+    $("#btnSave").on("click", () => {
+        saveData();
+    });
+    $("#btnDelete").on("click", () => {
+        deleteData();
+    });
+});
+
+clearInput = () => {
+    $("#hd-id").val("");
+    $("#booktitle").val("");
+    $("#author").val("");
+    $("#category").val("");
+    $("#isbn").val("");
+    $("#price").val("");
+    $("#condition").val("");
+    $("#myModal").val("");
+};
+
+saveData = () => {
+    const book = {
+        id: "",
+        title: $("#booktitle").val(),
+        author: $("#author").val(),
+        category: $("#category option:selected").text(),
+        isbn: $("#isbn").val(),
+        price: $("#price").val(),
+        condition: $("#condition option:selected").text()
+    };
+
+    if ($("#hd-id").val().length > 0) {
+        console.log("update");
+        book.id = $("#hd-id").val();
+        updateBook(book);
+    } else {
+        console.log("new");
+        Key = db.collection("service/").doc();
+        book.id = Key.id;
+        addBook(book);
+    }
+};
+
+addBook = book => {
+    console.log("adding book");
+    db.collection("book").doc(Key.id).set(book).then(() => {
+        displayToastMessage("book successfully added!");
+        getData();
+        $("#myModal").modal("toggle");
+    });
+    clearInput();
+};
+
+updateBook = book => {
+    db.collection("book").doc(book.id).update(book)
+        .then(() => {
+            getData();
+            $("#myModal").modal("toggle");
+        });
+};
+
+getData = () => {
+    const docRef = db.collection("book").orderBy("title");
+    docRef.get().then(docData => {
+        if (docData.size) {
+            const arrObj = [];
+            docData.forEach(data => {
+                const obj = data.data();
+                obj.id = data.id;
+                arrObj.push(obj);
+            });
+            dtTable.clear();
+            dtTable.rows.add(arrObj);
+            dtTable.draw();
+        } else {
+            console.log(docData);
+            dtTable.clear();
+            dtTable.draw();
+        }
+
+    }, error => {
+        console.log("error=", error);
+    });
+};
+
+deleteData = () => {
+    const id = $("#hd-id").val();
+    deleteDB(id);
+};
+
+deleteDB = id => {
+    db.collection("book").doc(id).delete()
+        .then(() => {
+            getData();
+            $("#myModal").modal("toggle");
+            displayToastMessage("Book deleted");
+        });
+};
+
+displayToastMessage = (message, error) => {
+    var x = document.getElementById("snackbar");
+    x.innerHTML = message;
+    // Add the "show" class to DIV
+    if (error) {
+        x.className = "show error";
+    } else {
+        x.className.replace("error", "");
+        x.className = "show";
+    }
+    setTimeout(() => { x.className = x.className.replace("show", ""); }, 3000);
+};
+
